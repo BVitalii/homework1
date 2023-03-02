96b58b03a3c12f667a311e95c6c4c40bec2fa18e
Vitalii Borodulin <borodulin.worldlcass@gmail.com>
2023-03-02 15:47:46 +0700

    сделал пока 2 задания из ВТОРОЯ ДОМАШНЕЙ РАБОТЫ

--- homework1/main.swift
+++ homework1/main.swift
@@ -10,16 +10,15 @@ import Foundation
 // 1.Написать код, который будет использовать 3 числа (Int, Float и Double) и выведет в консоль результат сложения всех трех чисел (Если число получается без остатка - написать Int результат. Если остаток имеется - записать в Float типе)
 
 var numInt: Int = 25
-var numFloat: Float = 25.4444444
-var numDouble: Double = 25.584920394
+var numFloat: Float = 25.125949
+var numDouble: Double = 25.5058398593
 
 var numResult = Float(numInt) + numFloat + Float(numDouble)
-for i in String(numResult) {
-    if i == "." {
-        print("Число \(numResult) является- Float")
-    } else {
-        print("Число \(numResult) является- Int")
-    }
+
+if numResult == Float(Int(numResult)){
+    print("...")
+}else{
+    print("ostatok")
 }
 
 // 2. Разобраться как работает приведение типов и попробовать конвертировать разные типы (например Int в String, Bool в String, String в Int)
@@ -43,9 +42,9 @@ var job = ""
 var genderM = true
 
 func personInfo() {
-    if genderM == true {
+    if genderM{
     print( "Его зовут \(name). Ему \(age). Он живет в \(city). Он работает в \(job)")
-    } else if genderM == false {
+    } else if !genderM{
         print( "Ee зовут \(name). Ей \(age). Она живет в \(city). Она работает в \(job)")
     }
 }
@@ -85,3 +84,53 @@ func sumString( text1: String, text2: String, text3: String)-> String {
 }
  var sumText = sumString(text1: "Петров", text2: "Иван", text3: "Васильевич")
 print(sumText)
+
+
+var nameArray = ["Alex","Berta","Mike","Maria"]
+
+var nameArray2 = ["Maria","Homer","Andrei","Julia", "Maria"]
+var nameArraysCount = 0
+for i in nameArray {
+    for i2 in nameArray2 {
+        if i == i2 {
+            nameArraysCount += 1
+        }
+    }
+    
+}
+print("всего \(nameArraysCount) совпадений")
+
+
+
+// ВТОРАЯ ДОМАШНЯЯ РАБОТА
+
+// составить программу, используя цикл, которая посчитает сколько чисел начинаются с "1" и "3" и заканчиваются на "5" в диапазоне 0...1000
+
+var numArray: [[String]] = [[]]
+for num in 0...1000 {
+    numArray.append([String(num)])
+}
+var index = 0
+for arr in numArray {
+    for str in arr {
+        if str[str.startIndex] == "1" ||  str[str.startIndex]  == "3" || str[str.index(before: str.endIndex)] == "5" {
+            index += 1
+            continue
+        }
+    }
+}
+print(index)
+
+
+// Используя условные операторы, функции и циклы создать следующую программу: Записать в одну переменную типа String 20 разных слов (с разной длиной) через запятую После того, как созданная ранее функция примет как аргумент созданную строку - вернет следующее (например) - “Яблоко - 6 символов \n Морковь - 7 символов” и т.д до последнего слова
+
+var words = "Яблоко,машина,стол,балкон,ручка,кухня,книга,кнопка,зеркало,телефон,лампа,бумага,шнур,обои,шторы,сумка,цифра,квадрат,звонок,сандали"
+
+func checkCount( word: String) {
+    let str = (word.components(separatedBy: ","))
+    for i in str {
+        print("\(i) - \(i.count) символов")
+    }
+}
+checkCount(word: words)
+
